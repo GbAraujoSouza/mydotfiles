7d8c59c30c3f5034c7e365fd0c0552c5db56fe2f
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-08-18 18:01:49 -0300

    feat: autoclosing tags feature

--- nvim/.config/nvim/lua/gbaraujosouza/plugins/autoclose.lua
+++ nvim/.config/nvim/lua/gbaraujosouza/plugins/autoclose.lua
@@ -4,3 +4,5 @@ return {
     require('autoclose').setup()
   end
 }
+
+
--- nvim/.config/nvim/lua/gbaraujosouza/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/gbaraujosouza/plugins/treesitter.lua
@@ -1,13 +1,49 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
     build = ":TSUpdate",
     config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
+      local treesitter = require("nvim-treesitter.configs")
+
+      treesitter.setup({
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
+
+
+        ensure_installed = {
+          "json",
+          "javascript",
+          "typescript",
+          "tsx",
+          "yaml",
+          "html",
+          "css",
+          "prisma",
+          "markdown",
+          "markdown_inline",
+
+          "bash",
+          "lua",
+          "vim",
+          "dockerfile",
+          "gitignore",
+          "query",
+          "vimdoc",
+          "c",
+          "java"
+        },
+
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
       })
     end
   }
