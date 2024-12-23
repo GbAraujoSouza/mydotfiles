ee8ff11255cbe34cada7801d3c76e96ce06d7dbf
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-12-23 10:59:22 -0300

    fix: fix size of neotree and show gitignored files

--- nvim/.config/nvim/lua/gbaraujosouza/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/gbaraujosouza/plugins/neo-tree.lua
@@ -10,11 +10,15 @@ return {
     vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
     vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
     require('neo-tree').setup {
+      window = {
+        position = "left",
+        width = 25,
+      },
       filesystem = {
         filtered_items = {
           visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
           hide_dotfiles = false,
-          hide_gitignored = true,
+          hide_gitignored = false,
         },
       }
     }
