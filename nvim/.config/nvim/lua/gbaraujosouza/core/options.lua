c531ca918c6d840810b9be79ad94bac37ddb84ca
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2025-05-18 16:09:55 -0300

    refactor: change neovim explorer style

--- nvim/.config/nvim/lua/gbaraujosouza/core/options.lua
+++ nvim/.config/nvim/lua/gbaraujosouza/core/options.lua
@@ -2,7 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.cmd("let g:netrw_liststyle = 3")
+vim.cmd("let g:netrw_liststyle = 1")
 vim.g.mapleader = " "
 -- vim.g.background = "light"
 vim.wo.number = true
